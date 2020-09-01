4357664bd627c3af5ceb027384b4dd5266d848ec
NohaaAa <NohaaAa>
2020-09-01 22:04:51 +0200

    Added QuestionCard Component

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { PropTypes } from 'prop-types';
+
+import {
+    Segment, Header, Grid, Image
+} from 'semantic-ui-react'
+
+class QuestionCard extends Component {
+
+    static propTypes = {
+        userId: PropTypes.string.isRequired,
+        color: PropTypes.string
+    }
+
+    render() {
+
+        const {user, child, color} = this.props
+        console.log((this.props))
+        return (
+            <Segment>
+                <Header 
+                    as='h5'
+                    textAlign='left'
+                    block
+                    attached='top'
+                    style={{borderTop: `2px solid ${color}`}}
+                    content={`${user.name} asks: `}
+                />
+
+                <Grid divided padded>
+                    <Grid.Row>
+                        <Grid.Column width={5}>
+                            <Image src={user.avatarURL}/>
+                        </Grid.Column>
+                        <Grid.Column width={11}>
+                            {child}
+                        </Grid.Column>
+                    </Grid.Row>
+
+                </Grid>
+               
+            </Segment>
+        )
+    }
+}
+
+function mapStateToProps({users}, props) {
+    const user = users[props.userId]
+    
+    return {
+        user
+    }
+}
+export default connect(mapStateToProps)(QuestionCard)
+    
