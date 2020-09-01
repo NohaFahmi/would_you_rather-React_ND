d7a3bd91ceb6188dc7cf6e4b0b757c82b09807d1
NohaaAa <NohaaAa>
2020-09-01 23:49:09 +0200

    Added Question && Results Components

---
+++ src/components/Question/Question.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
+
+
+class Question extends Component {
+    render() {
+        return (
+            <div>
+                This is a question main page
+            </div>
+        )
+    }
+}
+
+
+export default Question
---
+++ src/components/Question/Results.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
+
+
+class Results extends Component {
+    render() {
+        return (
+            <div>
+                This is a Results main page
+            </div>
+        )
+    }
+}
+
+
+export default Results
