fefa35916faf91083960456cff957463046df00a
NohaaAa <NohaaAa>
2020-09-02 02:47:41 +0200

    handle answer poll actions

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions';
+import { RECEIVE_QUESTIONS, ADD_ANSWER } from '../actions/questions';
 
 
 export default function questions(state = {}, action) {
@@ -7,6 +7,21 @@ export default function questions(state = {}, action) {
             return {
                 ...state,
                 ...action.questions
+            }
+
+        case ADD_ANSWER :
+
+        const {authedUser, qid, answer} = action
+        const votes = state[qid][answer]
+            return {
+                ...state,
+                [qid]: {
+                    ...state[qid],
+                    [answer]: {
+                        ...state[qid][answer],
+                        votes: votes.concat(authedUser)
+                    }
+                }
             }
         
         default : 
